2ac7a4ae754ed34468ad088efb6d84bb81592c9a
Migue12321 <miguel12321@gmail.com>
2018-11-26 07:45:21 -0400

    fix test atdd

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,7 +1,6 @@
   Given("visito la pagina de juego") do
       visit('/game')
   end
-   
 
   Given("Given visito la pagina de gameSettings") do
      visit("/gameSettings")
@@ -11,6 +10,7 @@
      fill_in('username1', with: string)
   end  
  
+  
   
   Then("deberia mostrar el nombre {string}") do |string|
     expect(page).to have_content(string) 
@@ -27,30 +27,28 @@
   Then("deberia mostrar el nombre con el color {string}") do |string|
    page.assert_all_of_selectors('h5.'+string) 
   end        
-   
+
   Then("deberia mostrar {string}") do |string|
     expect(page).to have_content("Migue: 0pts") 
   end
 
-   Given("visito la pagina de {string}") do |string|
+
+  Given("visito la pagina de {string}") do |string|
     visit(string)
   end
-  
+
   Given("selecciono un tablero de {int}") do |int|
     fill_in("size",with: int)
   end
-
+  
   When("presione el boton {string} que me llevo a la pagina \/game") do |string|
     click_button(string)
   end
 
   When("aprieto el boton {string} de la primera casilla") do |string|
     click_button(string)
-    end
-
-  Then("marca una linea {string} en la parte superior de la casilla") do |string|
-  expect(page).to have_css('#11up', :visible => false) 
   end
-  
-  
-  
+    
+  Then("marca una linea {string} en la parte superior de la casilla") do |string|
+    page.assert_all_of_selectors('div#11 style="border-top-color': 'black"') 
+  end
