607d34aa2a693e9fa15ca9a0ea4e47ed01465807
Migue12321 <miguel12321@gmail.com>
2018-11-25 23:56:34 -0400

    change name of scenario

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,10 +1,7 @@
   Given("visito la pagina de juego") do
       visit('/game')
   end
-  Given("visito la pagina de {string}") do |string|
-    visit(string)
-  end
-    
+   
 
   Given("Given visito la pagina de gameSettings") do
      visit("/gameSettings")
@@ -14,9 +11,6 @@
      fill_in('username1', with: string)
   end  
  
-  When("presione el boton {string} que me llevo a la pagina \/game") do |string|
-    click_button(string)
-  end
   
   Then("deberia mostrar el nombre {string}") do |string|
     expect(page).to have_content(string) 
@@ -33,25 +27,30 @@
   Then("deberia mostrar el nombre con el color {string}") do |string|
    page.assert_all_of_selectors('h5.'+string) 
   end        
-  Given("visito la pagina de {string} para seleccionar un tablero de {int}") do |string, int|
-    visit(string)
-    fill_in("size",with: int)
-    click_button("Empezar")
-  end
-  
-  When("aprieto el boton {string} de la primera casilla") do |string|
-  click_button(string)
-  end
-  
-  Then("marca una linea en la parte superior de la casilla") do
-      page.assert_all_of_selectors('button#11style="border-top-color': 'black"') 
+   
+  Then("deberia mostrar {string}") do |string|
+    expect(page).to have_content("Migue: 0pts") 
   end
 
+   Given("visito la pagina de {string}") do |string|
+    visit(string)
+  end
+  
   Given("selecciono un tablero de {int}") do |int|
     fill_in("size",with: int)
   end
 
-  Then("deberia mostrar {string}") do |string|
-    expect(page).to have_content("Migue: 0pts") 
-
+  When("presione el boton {string} que me llevo a la pagina \/game") do |string|
+    click_button(string)
   end
+
+  When("aprieto el boton {string} de la primera casilla") do |string|
+    click_button(string)
+    end
+
+  Then("marca una linea {string} en la parte superior de la casilla") do |string|
+  expect(page).to have_css('#11up', :visible => false) 
+  end
+  
+  
+  
